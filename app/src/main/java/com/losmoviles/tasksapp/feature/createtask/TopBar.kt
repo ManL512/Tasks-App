eadb88bb9da244ad5a316874d180be32183f19a8
ManL512 <tu_correo@dominio.com>
2025-10-07 17:02:45 -0500

    modular code

---
+++ app/src/main/java/com/losmoviles/tasksapp/feature/createtask/TopBar.kt
@@ -0,0 +1,34 @@
+package com.losmoviles.tasksapp.feature.createtask
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CreateTaskTopBarUi() {
+    TopAppBar(
+        title = {
+            Row(
+                Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                Text("Create Task", style = MaterialTheme.typography.titleMedium)
+                Icon(Icons.Filled.Settings, contentDescription = "Settings")
+            }
+        }
+    )
+}
