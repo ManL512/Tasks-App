ed3fbeb0afff9b7afb5c459e4de3ff586aeec572
ManL512 <tu_correo@dominio.com>
2025-10-08 15:59:27 -0500

    UI changes

--- app/src/main/java/com/losmoviles/tasksapp/screens/HomeScreen.kt
+++ app/src/main/java/com/losmoviles/tasksapp/screens/HomeScreen.kt
@@ -1,14 +1,22 @@
 package com.losmoviles.tasksapp.screens
 
 import android.net.Uri
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -16,7 +24,15 @@ import androidx.navigation.NavController
 fun HomeScreen(navController: NavController) {
     CustomScreen(
         title = "Inicio",
-        content = { ContentHomeScreen(navController) }
+        content = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFFA3485A))
+            ) {
+                ContentHomeScreen(navController)
+            }
+        }
         // Sin onTap: en Home no mostramos botón de atrás
     )
 }
@@ -27,11 +43,21 @@ fun ContentHomeScreen(navController: NavController) {
         modifier = Modifier
             .fillMaxSize()
             .padding(12.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Home")
+        Text(text = "Home",
+            color = Color(0xFFF5DAA7)
+        )
 
         Button(
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .width(100.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color(0xFF662222),
+                containerColor = Color(0xFFF5DAA7) // el primer par (FF) es la opacidad (alpha)
+            ),
             onClick = {
                 val title = "Mis tareas de hoy"
                 val safe = Uri.encode(title)
@@ -42,23 +68,37 @@ fun ContentHomeScreen(navController: NavController) {
         }
 
         Button(
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .width(100.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color(0xFF662222),
+                containerColor = Color(0xFFF5DAA7) // el primer par (FF) es la opacidad (alpha)
+            ),
             onClick = {
                 val title = "Mis tareas de hoy"
                 val safe = Uri.encode(title)
                 navController.navigate("detail-screen/$safe")
             }
         ) {
-            Text("Food")
+            Text("Idea")
         }
 
         Button(
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .width(100.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color(0xFF662222),
+                containerColor = Color(0xFFF5DAA7) // el primer par (FF) es la opacidad (alpha)
+            ),
             onClick = {
                 val title = "Mis tareas de hoy"
                 val safe = Uri.encode(title)
                 navController.navigate("detail-screen/$safe")
             }
         ) {
-            Text("Work")
+            Text("Idea")
         }
     }
 }
