0f1c495d23cc908b39e92f483706713be0ed5ace
ManL512 <tu_correo@dominio.com>
2025-10-08 08:55:34 -0500

    Main Screen - Modular features

--- app/src/main/java/com/losmoviles/tasksapp/feature/createtask/CreateTaskScreen.kt
+++ app/src/main/java/com/losmoviles/tasksapp/feature/createtask/CreateTaskScreen.kt
@@ -15,8 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.losmoviles.tasksapp.ActivityCardUi
-import com.losmoviles.tasksapp.DayChipUi
+
 import com.losmoviles.tasksapp.activitiesMock
 import com.losmoviles.tasksapp.daysMock
 
