541df371cd34f99a3eedeeaf32acb91037ab9bab
ManL512 <tu_correo@dominio.com>
2025-10-16 15:35:07 -0500

    now the route is the same title, as it should be

--- app/src/main/java/com/losmoviles/tasksapp/model/DataClass.kt
+++ app/src/main/java/com/losmoviles/tasksapp/model/DataClass.kt
@@ -15,18 +15,17 @@ import androidx.compose.material.icons.rounded.Favorite
 
 
 val activitiesMock = listOf(
-    ActivityUi("Texts",  "13 on this week", Icons.Outlined.Message),
-    ActivityUi("Buttons",  "8 on this week",  Icons.Outlined.AdsClick),
-    ActivityUi("Switch",  "2 on this week", Icons.Outlined.ChangeCircle),
-    ActivityUi("Radio Button",   "13 on this week", Icons.Filled.Radio),
-    ActivityUi("CheckBox", "8 on this week",  Icons.Rounded.CheckBox),
-    ActivityUi("Floating Action Button",   "15 on this week", Icons.Outlined.AirplanemodeActive),
+    ActivityUi("Texts", Icons.Outlined.Message),
+    ActivityUi("Buttons", Icons.Outlined.AdsClick),
+    ActivityUi("Switch", Icons.Outlined.ChangeCircle),
+    ActivityUi("Radio Button", Icons.Filled.Radio),
+    ActivityUi("CheckBox", Icons.Rounded.CheckBox),
+    ActivityUi("Floating Action Button", Icons.Outlined.AirplanemodeActive),
 )
 
 // -------------------- DATA CLASS (estático, sin lógica) --------------------
 
 data class ActivityUi(
-    val title: String,
     val route: String,
     val icon: androidx.compose.ui.graphics.vector.ImageVector
 )
